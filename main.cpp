b15723e3aca309a1cb24eefc69cb36f95eae334e
Pavel Nikolaev <Pavel.Nikolaev@t-systems.com>
2022-08-18 17:48:04 +0300

    add Users

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "metadata.h"
 
 //#include <filesystem>
+#include "user.h"
 
 int main()
 {
@@ -27,6 +28,15 @@ int main()
     std::cout << "Free space in meta: " << meta.free_space() << std::endl;
     std::cout << "size of meta: " << meta.size() << std::endl;
     meta.info();
+
+    My::Root &root = My::Root::Instance();
+    if(root.is_root())
+    {
+        std::cout << "Hello, I am root!" << std::endl;
+    }
+    std::shared_ptr<My::User> currentUser = root.createUser("pnikolae");
+    std::cout << "Hello, I am " << currentUser->name() << std::endl;
+    
 
     return 0;
 }
