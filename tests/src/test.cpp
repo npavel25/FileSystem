af36fe9f609be5c74debded24181f87c3a192cee
Pavel Nikolaev <Pavel.Nikolaev@t-systems.com>
2022-08-19 17:47:30 +0300

    add write-file test

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -1,10 +1,14 @@
 #include "gtest/gtest.h"
 #include "buddy.h"
+
+#include "file.h"
 #include "fs.h"
+#include "user.h"
 
 #include <fstream>
 //#include <filesystem>
 #include <string>
+
 TEST(FileSystem_test, create_test)
 {
   static const std::string mypath = "C:\\Users\\pnikolae\\Documents\\LevelUP\\CppII\\myfs\\testfilesys2";
@@ -48,9 +52,27 @@ TEST(FileSystem_test, read_test)
   EXPECT_TRUE(fs2.read());
 
   fs2.destroy();
-  
+
   std::ifstream infile3(mypath + ".myfs");
   EXPECT_FALSE(infile3.good());
+
+}
+
+TEST(File_test, write_test)
+{
+  static const std::string mypath = "C:\\Users\\pnikolae\\Documents\\LevelUP\\CppII\\myfs\\testfilesys2write";
+  
+  My::FileSystem fs(mypath, "myfs");
+  fs.create();
+  My::User user("test");
+  std::shared_ptr<My::File> file1 = fs.create_file(user, "file1");
+  file1->write("aaa", 3);
+  
+  std::shared_ptr<My::File> file2 = fs.create_file(user, "file2");
+  file2->write("bbbb", 4);
+
+  file1->flush();
+  file2->flush();
 
 }
 /*
